663e70221e035546ae9ad10848d81316517b6933
RinalinDS <denispilyutin@gmail.com>
2022-12-31 00:20:26 +0200

    public routes is useless as i suppose , but i'll keep them for some day

--- src/components/Routes/Routes.tsx
+++ src/components/Routes/Routes.tsx
@@ -9,14 +9,21 @@ import { View } from '../../pages/View/View';
 
 import styles from './Routes.module.css';
 
+// const PublicRoutes = [
+//   <Route path={Path.Home} element={<List />} />,
+//   <Route path={Path.City} element={<View />} />,
+//   <Route path={Path.ErrorPage} element={<ErrorPage />} />,
+//   <Route path={Path.AnyOther} element={<Navigate to={Path.ErrorPage} replace/>} />,
+// ];
+
 export const RoutesComponent = () => {
   return (
     <div className={styles.container}>
       <Routes>
-        <Route path={Path.Home} element={<List />} />
-        <Route path={Path.City} element={<View />} />
-        <Route path={Path.ErrorPage} element={<ErrorPage />} />
-        <Route path={Path.AnyOther} element={<Navigate to={Path.ErrorPage} />} />
+        <Route path={Path.Home} element={<List />} />,
+        <Route path={Path.City} element={<View />} />,
+        <Route path={Path.ErrorPage} element={<ErrorPage />} />,
+        <Route path={Path.AnyOther} element={<Navigate to={Path.ErrorPage} replace />} />,
       </Routes>
     </div>
   );
