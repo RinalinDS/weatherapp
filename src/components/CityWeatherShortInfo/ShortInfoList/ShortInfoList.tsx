611aaad9b23883f528cd49ab03b867837b9b7435
DenisPilyutin <denispilyutin@gmail.com>
2023-02-21 21:13:07 +0200

    feature: implementing useInternationalizedData hook results to make data universal

--- src/components/CityWeatherShortInfo/ShortInfoList/ShortInfoList.tsx
+++ src/components/CityWeatherShortInfo/ShortInfoList/ShortInfoList.tsx
@@ -3,37 +3,33 @@ import React, { FC } from 'react';
 import generalStyles from '../../../common/styles.module.css';
 import { CurrentForecastType } from '../../../types/CurrentForecastType';
 import { HourlyForecastList } from '../../../types/DailyForecastType';
+import { useInternationalizedData } from '../../../hooks/useInternationalizedData';
 
 type ShortInfoListPropsType = {
   forecastForCity: CurrentForecastType | HourlyForecastList;
 };
 
 export const ShortInfoList: FC<ShortInfoListPropsType> = ({ forecastForCity }) => {
+  const { temp, windSpeed, feelsLike, pressure } =
+    useInternationalizedData(forecastForCity);
+
   return (
     <ul className={generalStyles.list}>
       <li>
         <b>Temperature:</b>
-        <span className={generalStyles.padding}>
-          {Math.round(forecastForCity?.main?.temp)} °C
-        </span>
+        <span className={generalStyles.padding}>{temp}</span>
       </li>
       <li>
         <b>Feels like:</b>
-        <span className={generalStyles.padding}>
-          {Math.round(forecastForCity?.main?.feels_like)} °C
-        </span>
+        <span className={generalStyles.padding}>{feelsLike}</span>
       </li>
       <li>
         <b>Wind speed:</b>
-        <span className={generalStyles.padding}>
-          {Math.round(forecastForCity?.wind?.speed)} m/s
-        </span>
+        <span className={generalStyles.padding}>{windSpeed}</span>
       </li>
       <li>
         <b>Pressure:</b>
-        <span className={generalStyles.padding}>
-          {Math.round(forecastForCity?.main?.pressure)} mm
-        </span>
+        <span className={generalStyles.padding}>{pressure}</span>
       </li>
     </ul>
   );
